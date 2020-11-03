f3fe2bf175fc215c759401509b5e5bf051f7c585
Johannes Messner <messnerjo@gmail.com>
2020-11-03 17:43:34 +0000

    Add data-size vs error plot to judge overfitting

--- linreg.py
+++ linreg.py
@@ -14,8 +14,8 @@ def main():
     X_test = X[:N_test]
     y_test = y[:N_test]
 
-    # visualisatin of the data
-    #draw_bar(y_train)
+    # visualisation of the data
+    draw_bar(y_train)
     # baseline performance
     print("Baseline performance error: " + str(compute_baseline(y_train, y_test)))
 
@@ -23,8 +23,28 @@ def main():
     X_train_st, std, mean = standardize_training(X_train)
     X_test_st =  standardize_testing(X_test, std, mean)
     # predicting
-    mse = linear_prediction_error(X_train_st, y_train, X_test_st, y_test)
-    print("Least Squares prediction error: " + str(mse))
+    mse_train, mse_test = linear_prediction_error(X_train_st, y_train, X_test_st, y_test)
+    print("Least Squares training error: " + str(mse_train))
+    print("Least Squares test error: " + str(mse_test))
+
+    learning_curve(X_train_st, y_train, X_test_st, y_test)
+
+
+def learning_curve(X_train, y_train, X_test, y_test):
+    train_errors, test_errors = [], []
+    size_range = range(20, 601, 20)
+    for dataset_size in size_range:
+        mse_train, mse_test = linear_prediction_error(X_train[0:dataset_size, :], y_train[0:dataset_size], X_test, y_test)
+        train_errors.append(mse_train)
+        test_errors.append(mse_test)
+    errors = {'dataset size': size_range,
+              'training error': train_errors,
+              'test error': test_errors}
+    plt.plot('dataset size', 'training error', data=errors)
+    plt.plot('dataset size', 'test error', data=errors)
+    plt.xlabel('training dataset size')
+    plt.ylabel('MSE for training and test data')
+    plt.show()
 
 
 def standardize_training(X_train):
@@ -40,12 +60,16 @@ def standardize_testing(X_test, std, mean):
 
 
 def linear_prediction_error(X_train, y_train, X_test, y_test):
+    # add 1's to the beginning of every feature vector to account for bias term
+    X_train_prime = np.insert(X_train, 0, values=np.ones(len(X_train)), axis=1)
+    X_test_prime = np.insert(X_test, 0, values=np.ones(len(X_test)), axis=1)
     # calculate weight vector using closed form solution
-    w = np.linalg.inv((X_train.transpose() @ X_train))  @ X_train.transpose() @ y_train
-    # apply linear prediction to test set
-    y_test_hat = X_test @ w
-    # compute mean squared error
-    return np.mean((y_test - y_test_hat) ** 2) / len(y_test)
+    w = np.linalg.inv((X_train_prime.transpose() @ X_train_prime))  @ X_train_prime.transpose() @ y_train
+    # apply linear prediction to test set and training set
+    y_test_hat = X_test_prime @ w
+    y_train_hat = X_train_prime @ w
+    # compute mean squared error for training and test set
+    return np.mean((y_train - y_train_hat) ** 2), np.mean((y_test - y_test_hat) ** 2)
 
 
 def compute_baseline(y_train, y_test):
